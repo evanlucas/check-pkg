e45832561def95b3d33e61c073a13bc752eb13b6
Evan Lucas <evanlucas@me.com>
2016-05-16 19:10:36 -0500

    index: make sure we work for scoped nested requires

---
+++ test/scoped.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const tap = require('tap')
+
+if (require.main === module) {
+  tap.pass('skip')
+  return
+}
+
+require('@helpdotcom/biscuits')
+require('@helpdotcom/biscuits/thing')
+require('@helpdotcom/biscuit/thing')
--- test/test.js
+++ test/test.js
@@ -20,9 +20,34 @@ test('Check', (t) => {
 
   const check = new Check({
     root: root
+  , fileFilter: (entry) => {
+      if (entry.name === 'scoped.js' || path.extname(entry.path) !== '.js')
+        return false
+
+      return true
+    }
   })
 
   check.on('module', (mod) => {
     t.equal(mod.errors.length, 0, `${mod.relativePath} no errors`)
+  })
+})
+
+test('Check scoped', (t) => {
+  t.plan(4)
+  const check = new Check({
+    root: root
+  , package: path.join(__dirname, 'tpkg.json')
+  , fileFilter: (entry) => {
+      return entry.name === 'scoped.js'
+    }
+  })
+
+  check.on('module', (mod) => {
+    t.equal(mod.relativePath, 'test/scoped.js')
+    t.equal(mod.errors.length, 1)
+    const e = mod.errors[0]
+    t.equal(e.type, 'module')
+    t.equal(e.dependency, '@helpdotcom/biscuit/thing')
   })
 })
